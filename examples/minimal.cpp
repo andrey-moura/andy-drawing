dcecdc2e0724b91c715aa145e7f552af1450deb8
Andrey Moura <andreymds88@gmail.com>
2024-12-23 22:17:32 -0300

    internal: Some changes

--- examples/minimal.cpp
+++ examples/minimal.cpp
@@ -1,3 +1,24 @@
+#include <uva/drawing.hpp>
+
+#include <chrono>
+#include <iostream>
+
 int main() {
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+    uva::drawing::memory_surface surface(4000, 4000);
+    uva::drawing::software_renderer renderer(surface);
+
+    renderer.clear(uva::color(0, 255, 0));
+
+    std::filesystem::path path = std::filesystem::current_path();
+    surface.write_to_file(path, "output");
+
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+
+    std::cout << "Duration: " << duration.count() << " seconds" << std::endl;
+
     return 0;
 }
