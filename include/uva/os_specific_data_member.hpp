ec1ecfb4669171338c26b810e322220c7758478b
Andrey Moura <andreymds88@gmail.com>
2024-12-31 21:28:51 -0300

    wip: Small change

--- include/uva/os_specific_data_member.hpp
+++ include/uva/os_specific_data_member.hpp
@@ -15,7 +15,7 @@ public:
     template<typename T>
     const T& os_specific_data_as() const {
         static_assert(sizeof(T) <= DATA_SIZE, "in os_specific_data_as sizeof(T) is greater than DATA_SIZE");
-        return *(reinterpret_cast<T*>(m_os_specific_data));
+        return *(reinterpret_cast<const T*>(m_os_specific_data));
     }
     template<typename T>
     T& os_specific_data_as() {
