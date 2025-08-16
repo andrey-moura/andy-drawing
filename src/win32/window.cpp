983692d7e3a219f03f11f617331fc084a4cac6c5
Andrey Moura <andreymds88@gmail.com>
2025-08-16 17:10:27 -0300

    [WIP] Add WIN32 backend

---
+++ src/win32/window.cpp
@@ -0,0 +1,70 @@
+#include "andy/drawing/window.hpp"
+
+#include <Windows.h>
+
+auto to_tstring(std::string_view str)
+{
+#ifdef UNICODE
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0);
+    std::wstring wstr(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), &wstr[0], size_needed);
+    return wstr;
+#else
+    return str;
+#endif
+}
+
+static HWND create_window(std::string_view title)
+{
+    // Register the window class
+    const TCHAR CLASS_NAME[] = TEXT("Window Class");
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = DefWindowProc;
+    wc.hInstance     = GetModuleHandle(NULL);
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,                                // Optional window styles.
+        CLASS_NAME,                       // Window class
+        to_tstring(title).data(),         // Window text
+        WS_OVERLAPPEDWINDOW,              // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        GetModuleHandle(NULL),  // Instance handle
+        NULL        // Additional application data
+    );
+
+    return hwnd;
+}
+
+#define m_hwnd (HWND)m_data
+
+namespace andy
+{
+    namespace drawing
+    {
+        window::window(std::string_view title)
+        {
+            // Create the window using GTK
+            m_data = create_window(title);
+        }
+
+        window::~window()
+        {
+            if (m_hwnd)
+            {
+                DestroyWindow(m_hwnd);
+                m_data = nullptr;
+            }
+        }
+    };
+};
